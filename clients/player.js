303d1992fa979ea2c00d6e3369e07390a999c56f
TYLER BENNETT <tyler.bennett52@gmail.com>
2023-03-10 00:24:01 -0600

    all rooms built

--- clients/player.js
+++ clients/player.js
@@ -5,14 +5,82 @@ const PORT = process.env.PORT || 3006;
 const { io } = require('socket.io-client');
 const socket = io(`http://localhost:${PORT}/room10`);
 const { prompt } = require('enquirer');
+const figlet = require('figlet')
 console.log(process.env.PORT);
 let timer = 5;
+let verifiedUser = {username: 'guest'}
 
 socket.on('main-menu', mainMenu);
-socket.on('start-game', async () => {
-  console.log('Get ready to begin');
+socket.on('start-game', async (user) => {
+  if (user) {
+    verifiedUser = user;
+  }
+  console.log(`Get ready to begin ${verifiedUser.username}`);
   setInterval(advanceTimer, 1000);
 });
+socket.on('game1', () => {
+  multipleChoice(1);
+});
+socket.on('game1-retake', () => {
+  multipleChoice(1);
+});
+socket.on('game2', () => {
+  multipleChoice(2);
+});
+socket.on('game2-retake', () => {
+  multipleChoice(2);
+});
+socket.on('game3', () => {
+  multipleChoice(3);
+});
+socket.on('game3-retake', () => {
+  multipleChoice(3);
+});
+socket.on('game4', () => {
+  multipleChoice(4);
+});
+socket.on('game4-retake', () => {
+  multipleChoice(4);
+});
+socket.on('game5', () => {
+  multipleChoice(5);
+});
+socket.on('game5-retake', () => {
+  multipleChoice(5);
+});
+socket.on('game6', () => {
+  multipleChoice(6);
+});
+socket.on('game6-retake', () => {
+  multipleChoice(6);
+});
+socket.on('game7', () => {
+  multipleChoice(7);
+});
+socket.on('game7-retake', () => {
+  multipleChoice(7);
+});
+socket.on('game8', () => {
+  multipleChoice(8);
+});
+socket.on('game8-retake', () => {
+  multipleChoice(8);
+});
+socket.on('game9', () => {
+  multipleChoice(9);
+});
+socket.on('game9-retake', () => {
+  multipleChoice(9);
+});
+socket.on('game10', () => {
+  multipleChoice(10);
+});
+socket.on('game10-retake', () => {
+  multipleChoice(10);
+});
+socket.on('winner', () => {
+  console.log('NEVER GONNA GIVE YOU UP');
+})
 
 async function mainMenu() {
   let { haveAccount } = await prompt({
@@ -55,9 +123,25 @@ async function getCredentials() {
 
 function advanceTimer() {
   if (timer > 0) {
+    console.clear();
     console.log(timer);
   }
   timer -= 1;
+}
+
+async function multipleChoice(roomNum) {
+  console.clear();
+  figlet(`Welcome to Room ${roomNum}`, (err, data) => {
+    console.log(data);
+});
+  let { answer } = await prompt({
+    type: 'input',
+    name: 'answer',
+    message: 'Hint: it\'s the first letter of the alphabet'
+  })
+  console.log(timer * -1);
+  let correctAnswer = 'a'
+  socket.emit(`answer${roomNum}`, answer, correctAnswer)
 }
 
 // async function signup
--- server/server.js
+++ server/server.js
@@ -26,22 +26,30 @@ room10.on('connection', (socket) => {
       if (user.password === hashedPassword) {
         console.log('Success, emit game start')
       }
+      socket.emit('start-game', user);
     } catch (error) {
       console.log(error);
+      socket.emit('main-menu')
     }
   })
   socket.on('create-account', async (credentials) => {
     
-
-    
     try {
+      let hashedPassword = await bcrypt.hash(credentials.password, 5);
       let newUser = await userModel.create({
         username: credentials.username,
         password: hashedPassword
       })
       console.log(newUser);
+      socket.emit('start-game', user);
+      setTimeout(() => {
+        socket.join('room1');
+        console.log(socket.id, 'has joined room1');
+      }, 5000);
+      socket.emit('game1');
+
     } catch (error) {
-      console.log('Do better')
+      console.log(error)
       socket.emit('main-menu');
     }
 
@@ -50,7 +58,115 @@ room10.on('connection', (socket) => {
 
   socket.on('continue-guest', () => {
     socket.emit('start-game');
+    socket.join('room1');
+    setTimeout(() => {
+      socket.emit('game1');
+    }, 5500);
   })
+
+
+  socket.on('answer1', (answer, correctAnswer) => {
+    console.log('answer1:', answer, 'Correct:', correctAnswer)
+    if (answer === correctAnswer) {
+      socket.leave('room1');
+      socket.join('room2');
+      socket.emit('game2');
+    } else {
+      socket.emit('game1-retake');
+    }
+  })
+  socket.on('answer2', (answer, correctAnswer) => {
+    console.log('answer2:', answer, 'Correct:', correctAnswer)
+    if (answer === correctAnswer) {
+      socket.leave('room2');
+      socket.join('room3');
+      socket.emit('game3');
+    } else {
+      socket.emit('game2-retake');
+    }
+  })
+  socket.on('answer3', (answer, correctAnswer) => {
+    console.log('answer3:', answer, 'Correct:', correctAnswer)
+    if (answer === correctAnswer) {
+      socket.leave('room3');
+      socket.join('room4');
+      socket.emit('game4');
+    } else {
+      socket.emit('game3-retake');
+    }
+  })
+  socket.on('answer4', (answer, correctAnswer) => {
+    console.log('answer4:', answer, 'Correct:', correctAnswer)
+    if (answer === correctAnswer) {
+      socket.leave('room4');
+      socket.join('room5');
+      socket.emit('game5');
+    } else {
+      socket.emit('game4-retake');
+    }
+  })
+  socket.on('answer5', (answer, correctAnswer) => {
+    console.log('answer5:', answer, 'Correct:', correctAnswer)
+    if (answer === correctAnswer) {
+      socket.leave('room5');
+      socket.join('room6');
+      socket.emit('game6');
+    } else {
+      socket.emit('game5-retake');
+    }
+  })
+  socket.on('answer6', (answer, correctAnswer) => {
+    console.log('answer6:', answer, 'Correct:', correctAnswer)
+    if (answer === correctAnswer) {
+      socket.leave('room6');
+      socket.join('room7');
+      socket.emit('game7');
+    } else {
+      socket.emit('game6-retake');
+    }
+  })
+  socket.on('answer7', (answer, correctAnswer) => {
+    console.log('answer7:', answer, 'Correct:', correctAnswer)
+    if (answer === correctAnswer) {
+      socket.leave('room7');
+      socket.join('room8');
+      socket.emit('game8');
+    } else {
+      socket.emit('game7-retake');
+    }
+  })
+  socket.on('answer8', (answer, correctAnswer) => {
+    console.log('answer8:', answer, 'Correct:', correctAnswer)
+    if (answer === correctAnswer) {
+      socket.leave('room8');
+      socket.join('room9');
+      socket.emit('game9');
+    } else {
+      socket.emit('game8-retake');
+    }
+  })
+  socket.on('answer9', (answer, correctAnswer) => {
+    console.log('answer9:', answer, 'Correct:', correctAnswer)
+    if (answer === correctAnswer) {
+      socket.leave('room9');
+      socket.join('room10');
+      socket.emit('game10');
+    } else {
+      socket.emit('game9-retake');
+    }
+  })
+  socket.on('answer10', (answer, correctAnswer) => {
+    console.log('answer10:', answer, 'Correct:', correctAnswer)
+    if (answer === correctAnswer) {
+      socket.leave('room10');
+      socket.join('champions');
+      socket.emit('winner');
+    } else {
+      socket.emit('game10-retake');
+    }
+  })
+
+
 })
 
 
