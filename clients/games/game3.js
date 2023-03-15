b1bf9132bea4b6570abaec7c37c3cb6a39b4ae64
Ty Aponte <aaustinsk8@aol.com>
2023-03-15 06:58:03 -0700

    refactored and corrected game 3 prompt

--- clients/games/game3.js
+++ clients/games/game3.js
@@ -15,10 +15,14 @@ async function game3(socket) {
     console.log(chalk.red(data))
 });
 
-let int1 = chance.integer({min: 1, max: 5 })
-let int2 = chance.integer({min: 1, max: 5 })
+// let int1 = chance.integer({min: 1, max: 5 })
+// let int2 = chance.integer({min: 1, max: 5 })
+
 let ans = chance.integer({min: 1, max: 5 })
-const colorArr = ['red', 'magenta', 'yellow', 'green', 'blue']
+let int1 = ans*2;
+// let int2 = ans*2;
+
+const colorArr = ['red', 'magenta', 'yellow', 'green', 'blue'];
 let correctAnswer = colorArr[+ans-1];
 
 let { answer } = await prompt({
@@ -26,7 +30,7 @@ let { answer } = await prompt({
   name: 'answer',
   message: `\nIn this paragraph you will see ${chalk.red('certain')} words in different colors. \nDisregarding ${chalk.magenta('words')} that are white, solve the following ${chalk.yellow('equation')} \nand with the answer count that amount of ${chalk.green('colored')} words from the beginning. \nType ${chalk.blue('the')} color of the word you land on: \n\n
 
-  Two cyclists start ${int1} miles apart, and begin cycling towards each other at ${int2} mph.\n
+  Two cyclists start ${int1} miles apart, and begin cycling towards each other at ${ans} mph.\n
   The instant they begin, a fly goes from one cyclist to the other, then back to the first cyclist,\n
   and so on, always flying between the cyclists. The fly flies at a constant ${ans} mph. How far in \n
   total does the fly travel when the cyclists meet? \n\n\n\n`,
