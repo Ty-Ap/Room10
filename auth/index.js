ccb22bdd233689e3629677c4b9c761d1ddfda951
Jordan Covington <Atlantafan1087@gmail.com>
2023-03-16 13:44:12 -0700

    altered DB_URL for testing

--- auth/index.js
+++ auth/index.js
@@ -3,14 +3,9 @@
 require('dotenv').config();
 const { Sequelize, DataTypes } = require('sequelize');
 const userSchema = require('./users-models');
-// const DB_URL = process.env.NODE_ENV === 'test' ? 'sqlite::memory' : process.env.DB_URL;
-const DB_URL = 'postgres://localhost:5432/room10';
-
+const DB_URL = process.env.NODE_ENV === 'test' ? 'sqlite::memory' : 'postgres://localhost:5432/room10';
+// const DB_URL = 'postgres://localhost:5432/room10';
 const db = new Sequelize(DB_URL);
 const userModel = userSchema(db, DataTypes);
-// sequelize allows ua to interact with the usermodel before adding data to the database using the beforeCreate hook.
-
-
-
 
 module.exports = { db, userModel };
