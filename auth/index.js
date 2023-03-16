3a98e41b2b5bf86f9f9f61cf3235902b70f649b3
TYLER BENNETT <tyler.bennett52@gmail.com>
2023-03-16 16:31:09 -0500

    db fix

--- auth/index.js
+++ auth/index.js
@@ -3,7 +3,7 @@
 require('dotenv').config();
 const { Sequelize, DataTypes } = require('sequelize');
 const userSchema = require('./users-models');
-const DB_URL = process.env.NODE_ENV === 'test' ? 'sqlite::memory' : 'postgres://localhost:5432/room10';
+const DB_URL = process.env.NODE_ENV === 'test' ? 'sqlite::memory' : 'postgres://bennett_401_labs_database_user:gQDAokLUlcgk7Ezciamwy8Bay6kmkQxO@dpg-cfn4i02rrk0eqlu8lun0-a/bennett_401_labs_database';
 // const DB_URL = 'postgres://localhost:5432/room10';
 const db = new Sequelize(DB_URL);
 const userModel = userSchema(db, DataTypes);
